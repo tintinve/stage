92f7c3176f2190343fdf3c862ad13835538d41be
tintinve <pedroluismejia@gmail.com>
2018-10-04 00:24:41 +0200

    V0.09

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let template = document.querySelector("#aPostTemplate").content;
-let eventlist = document.querySelector("#eventlist");
+let blog = document.querySelector("#blog");
 let page = 1;
 let lookingForData = false;
 let catLink = "https://eupeo.com/index.php/wp-json/wp/v2/categories";
@@ -17,116 +17,21 @@ function fetchData() {
 
 function showContent(data) {
   console.log(data);
-  lookingForData = false;
   data.forEach(showPost);
 }
-
-function showPost(anEvent) {
-  //console.log(anEvent._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url);
+function showPost(aPost) {
   let clone = template.cloneNode(true);
-  clone.querySelector("h1").textContent = anEvent.title.rendered;
-  let cat = anEvent.acf.event_type.replace(" ", "_");
-  clone.querySelector(".event").classList.add(cat);
-  clone.querySelector(".price span").textContent = anEvent.acf.price;
-  clone.querySelector(".category").textContent = anEvent.acf.event_type;
-  clone.querySelector(".venue").textContent =
-    "Location: " + anEvent.acf.location;
-  console.log(anEvent.acf.location);
-  let date = anEvent.acf.date;
-  clone.querySelector(".date").textContent =
-    "Date: " +
-    date.substring(0, 4) +
-    "/" +
-    date.substring(4, 6) +
-    "/" +
-    date.substring(6, 8);
-  let time = anEvent.acf.time;
-  clone.querySelector(".time").textContent =
-    "Time: " + time.substring(0, 2) + ":" + time.substring(2, 4);
-  clone
-    .querySelector("img")
-    .setAttribute(
-      "src",
-      anEvent._embedded["wp:featuredmedia"][0].media_details.sizes.medium
-        .source_url
-    );
-  console.log(
-    anEvent._embedded["wp:featuredmedia"][0].media_details.sizes.medium
-      .source_url
-  );
-  clone.querySelector(".readmore").href = "subpage.html?id=" + anEvent.id;
-  eventlist.appendChild(clone);
+  clone.querySelector("h1").textContent = aPost.title.rendered;
+  clone.querySelector(".readMore").textContent = aPost.id;
+  clone.querySelector(".readMore").href = "subpage.html?id=" + aPost.id;
+  blog.appendChild(clone);
 }
 
 fetchData();
-/*
-setInterval(function () {
 
-    if (bottomVisible() && lookingForData === false) {
-        console.log("Getting More Events")
-        page++;
-        fetchData();
-    }
-}, 100)
-*/
-function bottomVisible() {
-  const scrollY = window.scrollY;
-  const visible = document.documentElement.clientHeight;
-  const pageHeight = document.documentElement.scrollHeight;
-  const bottomOfPage = visible + scrollY >= pageHeight - 20;
-  return bottomOfPage || pageHeight < visible;
-}
-
-fetch(catLink)
-  .then(result => result.json())
-  .then(cats => sort(cats));
-
-function sort(cats) {
-  cats.forEach(cat => {
-    console.log(cat);
-    const a = document.createElement("a");
-    a.href = "#";
-    if (cat.count > 0) {
-      a.textContent = cat.name + " (" + cat.count + ")";
-    } else {
-      a.textContent = cat.name;
-    }
-    if (cat.name === "Supplement") {
-      a.style.display = "none";
-    }
-    a.classList.add("menu_item");
-    a.addEventListener("click", () => filter(cat));
-    aside.appendChild(a);
-  });
-}
-
-function filter(category) {
-  console.log(category.slug);
-  if ((document.querySelector("#loader").style.display = "none")) {
-    document.querySelector("#loader").style.display = "block";
-    myTimer;
-  }
-  // Esto es el primer paso para que Uncategorized muestre los eventos de Clemens y Karoline
-  if (category.slug === "uncategorized") {
-    console.log("EEE");
-  }
-  //document.querySelector(".burger").classList.remove("change")
-  document.querySelectorAll(".event").forEach(el => {
-    el.classList.add("hidden");
-    if (el.classList.contains(category.slug)) {
-      console.log("i have a class called " + category.slug);
-      el.classList.remove("hidden");
-    } else {
-      console.log("i DONT have a class called " + category.slug);
-    }
-    aside.classList.remove("traeMenu");
-    document.querySelector(".burger").classList.toggle("change");
-  });
-}
 function burger(x) {
   console.log(x);
   x.classList.toggle("change");
-  console.log("change");
 }
 
 document.querySelector(".burger").addEventListener("click", trae_menu);
--- js/subpage.js
+++ js/subpage.js
@@ -1,23 +1,18 @@
 let template = document.querySelector("#eventtemp").content;
 let urlParams = new URLSearchParams(window.location.search);
 let id = urlParams.get("id");
-console.log("el numero ganador es " + id);
-
-fetch("https://eupeo.com/index.php/wp-json/wp/v2/events/" + id)
-    .then(e => e.json())
-    .then(showSinglePost)
-
+fetch("https://eupeo.com/index.php/wp-json/wp/v2/posts/" + id)
+  .then(e => e.json())
+  .then(showSinglePost);
 function showSinglePost(anEvent) {
-    let clone = template.cloneNode(true);
-    clone.querySelector("h1").textContent = anEvent.title.rendered;
-    clone.querySelector(".description").innerHTML = anEvent.content.rendered;
-    clone.querySelector(".price span").textContent = anEvent.acf.price;
-    clone.querySelector(".category").textContent = anEvent.acf.event_type;
-    clone.querySelector(".venue").textContent = "Location: " + anEvent.acf.location;
-    clone.querySelector(".date").textContent = "Date: " + anEvent.acf.date;
-    clone.querySelector(".time").textContent = "Time: " + anEvent.acf.time;
-    eventlist.appendChild(clone);
-    
-} 
-
-
+  let clone = template.cloneNode(true);
+  clone.querySelector("h1").textContent = anEvent.title.rendered;
+  clone.querySelector(".description").innerHTML = anEvent.content.rendered;
+  clone.querySelector(".price span").textContent = anEvent.acf.price;
+  clone.querySelector(".category").textContent = anEvent.acf.event_type;
+  clone.querySelector(".venue").textContent =
+    "Location: " + anEvent.acf.location;
+  clone.querySelector(".date").textContent = "Date: " + anEvent.acf.date;
+  clone.querySelector(".time").textContent = "Time: " + anEvent.acf.time;
+  eventlist.appendChild(clone);
+}
