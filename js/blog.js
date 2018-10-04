68ec336853673df98a53bb87b15ec6d233a30a99
tintinve <pedroluismejia@gmail.com>
2018-10-05 01:50:29 +0200

    V0.22

--- js/blog.js
+++ js/blog.js
@@ -2,44 +2,11 @@ let template = document.querySelector("#aPostTemplate").content;
 let blog = document.querySelector("#blog");
 const aside = document.querySelector("aside");
 const mytimer = setInterval(myTimer, 10000);
-//------------------------>>>  Start Animation iPhone
-
-TweenMax.from("#message_box_phone", 2, { x: 300 });
-const all = document.querySelectorAll("#lineas path");
-const tl = new TimelineMax({ repeat: 0, repeatDelay: 1 });
-tl.staggerFrom(
-  all,
-  3,
-  {
-    x: () => Math.random() * 1000 - 500,
-    y: () => Math.random() * 1000 - 500
-  },
-  0.1
-);
-
-//------------------------>>> End Animation iPhone
-//------------------------>>>  Start Animation Bola
-const bola = document.querySelectorAll("#triangulos polygon");
-const tl1 = new TimelineMax({ repeat: 0, repeatDelay: 1 });
-tl1.staggerFrom(
-  bola,
-  6,
-  {
-    x: () => Math.random() * 1000 - 500,
-    y: () => Math.random() * 1000 - 500
-  },
-  0.1
-);
-//------------------------>>> End Animation Bola
 function fetchData() {
-  lookingForData = true;
-  fetch(
-    "https://eupeo.com/index.php/wp-json/wp/v2/posts?_embed&per_page=30&page=1"
-  )
+  fetch("https://eupeo.com/index.php/wp-json/wp/v2/posts")
     .then(e => e.json())
     .then(showContent);
 }
-
 function showContent(data) {
   console.log(data);
   data.forEach(showPost);
@@ -55,18 +22,15 @@ function showPost(aPost) {
 }
 
 fetchData();
-
+function myTimer() {
+  document.querySelector("#loader").style.display = "none";
+}
 function burger(x) {
   x.classList.toggle("change");
 }
-
 document.querySelector(".burger").addEventListener("click", trae_menu);
 
 function trae_menu() {
   document.querySelector("aside").classList.toggle("traeMenu");
   aside.style.display = "block";
-}
-
-function myTimer() {
-  document.querySelector("#loader").style.display = "none";
 }
