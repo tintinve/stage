14772a80ec132ce166d55d451639159523063b1d
tintinve <pedroluismejia@gmail.com>
2018-10-05 18:06:29 +0200

    V1.0

--- js/subpage.js
+++ js/subpage.js
@@ -8,11 +8,5 @@ function showSinglePost(anEvent) {
   let clone = template.cloneNode(true);
   clone.querySelector("h1").textContent = anEvent.title.rendered;
   clone.querySelector(".description").innerHTML = anEvent.content.rendered;
-  clone.querySelector(".price span").textContent = anEvent.acf.price;
-  clone.querySelector(".category").textContent = anEvent.acf.event_type;
-  clone.querySelector(".venue").textContent =
-    "Location: " + anEvent.acf.location;
-  clone.querySelector(".date").textContent = "Date: " + anEvent.acf.date;
-  clone.querySelector(".time").textContent = "Time: " + anEvent.acf.time;
   eventlist.appendChild(clone);
 }
